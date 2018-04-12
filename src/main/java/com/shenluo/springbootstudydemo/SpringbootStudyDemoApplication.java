607c2174cf6a6ffdb6563861c738395dba7835e9
web-shenluo <474047353@qq.com>
2018-04-12 23:03:52 +0800

    快速构建

---
+++ src/main/java/com/shenluo/springbootstudydemo/SpringbootStudyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.shenluo.springbootstudydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyDemoApplication.class, args);
+	}
+}
