2ced73ff11701de6a4772f1ac1e57847decef6cb
web-shenluo <474047353@qq.com>
2018-04-14 16:03:17 +0800

    返回一个user对象

--- src/main/java/com/shenluo/SpringbootStudyDemoApplication.java
+++ src/main/java/com/shenluo/SpringbootStudyDemoApplication.java
@@ -1,4 +1,4 @@
-package com.shenluo.springbootstudydemo;
+package com.shenluo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
